198a8af39521da7635ccdec0616788083d7d9de0
bojan987 <bojanmaj@hotmail.com>
2020-07-27 12:15:35 +0200

    pokusaj 4. zadatka 2 deo

--- 4.js
+++ 4.js
@@ -6,3 +6,14 @@ for (i=1; i<10;i++){
     proizvod = i  * cifra 
     console.log(proizvod)
 }
+//******************** */
+// dodatno 4. zadatak
+//******************* */
+// let proizvod = 0
+// for (i=1; i<10;i++){
+//     for (j=1;j<10;j++){
+//         proizvod = i*j
+    
+//     }
+    
+// }
