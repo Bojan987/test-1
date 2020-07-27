e0b66b1eb7364aca74f596cd46a825b428ae9a95
bojan987 <bojanmaj@hotmail.com>
2020-07-27 12:08:54 +0200

    prepravljen 2 zadatak

--- 2.js
+++ 2.js
@@ -8,46 +8,20 @@
 //    ***
 //     *
 
-let N = 5;
-// let space = N - 1;
-// for (i = 1; i <= 2 * N - 1; i += 2) {
-//   if (i < 9) {
-//     console.log(" ".repeat(space) + "*".repeat(i));
-//     space--;
-//   }
-//   //    console.log(i)
-//   else if (N % 2 !== 0) {
-//     console.log(
-//       " ".repeat(space) +
-//         "*".repeat(Math.floor(i / 2)) +
-//         " " +
-//         "*".repeat(Math.floor(i / 2))
-//     );
-//   }
-//   //    space --
-//   else if (N % 2 === 0) {
-      
-//     console.log(
-//       " ".repeat(space) +
-//         "*".repeat(Math.floor(i / 2)) +
-//         " " +
-//         "*".repeat(Math.floor(i / 2))
-//     );
-//      if (i=2*N){
-//           continue
-//      }
-     
-//   }
-// }
+let N = 6;
 
 for (i=1 ;i<=N;i++){
    if (i<N) console.log (" ".repeat(N-i) + "*".repeat(i) + "*".repeat(i-1))
 
-   else if (i===2*N) {
-    console.log (" ".repeat(N-i) + "*".repeat(Math.floor(i/2)) + " " +"*".repeat(Math.floor(i/2)))
+   else if (i===N) {
+    console.log (" ".repeat(N-i) + "*".repeat(Math.floor(i-1)) + " " +"*".repeat(i-1))
    }
 
 
 }
-// for (i=N)
 
+
+for (i = N; i>0; i--){
+ 
+    console.log (" ".repeat(N-i) + "*".repeat(i) + "*".repeat(i-1))
+}
